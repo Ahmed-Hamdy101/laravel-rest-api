62fe85bac8716914d6017d3028a28d48602d8d95
Ahmed-Hamdy101 <ahmedhamdy.mh95@gmail.com>
2026-07-11 19:15:50 +0300

    feat: implement role-based user creation in userSeeder

--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,17 @@ class userSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        $adminRole = Role::whereName('Admin')->first();
+        $editorRole = Role::whereName('Editor')->first();
+        $userRole = Role::whereName('User')->first();
+
+        // Create admin user
+        User::factory(1)->create(['role_id' => $adminRole->id]);
+
+        // Create editor users
+        User::factory(3)->create(['role_id' => $editorRole->id]);
+
+        // Create regular users
+        User::factory(6)->create(['role_id' => $userRole->id]);
     }
 }
