97f2f938f46bc1bd129cb49ca069086d022f2a81
Ahmed-Hamdy101 <ahmedhamdy.mh95@gmail.com>
2026-07-11 19:14:56 +0300

    refactor: comment out unused exception handling logic for clarity 🔴

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -22,9 +22,13 @@ class Handler extends ExceptionHandler
      * Register the exception handling callbacks for the application.
      */
     public function register(): void
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+     {
+        // $this->renderable(function (Throwable $e, $request) {
+ 
+        //     return response([
+        //         'error' => $e->getMessage(),
+        //     ], $e->getCode() ? $e->getCode() : 400
+        //     );
+        // });
     }
 }
