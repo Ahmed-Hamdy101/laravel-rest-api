baad59cb7d8abd45bc8f70f6ddfe433579bf3c68
Ahmed-Hamdy101 <ahmedhamdy.mh95@gmail.com>
2026-09-13 16:54:52 +0300

    feat: add OpenAPI annotations to CreateUserRequest for improved API documentation

--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -3,12 +3,49 @@
 namespace App\Http\Requests;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Http\FormRequest;
+use OpenApi\Attributes as OA;
 
+#[OA\Schema(
+    title: "Create New User ",
+    description: "Create New Record",
+)]
 class CreateUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
+    #[OA\Property(
+        title: "f_name",
+        type: "string"
+    )]
+    public $f_name;
+
+    #[OA\Property(
+        title: "l_name",
+        type: "string"
+    )]
+    public $l_name;
+
+    #[OA\Property(
+        title: "email",
+        type: "string"
+    )]
+    public $email;
+    
+    #[OA\Property(
+        title: "password",
+        type: "string"
+    )]
+
+    public $password;
+    
+    #[OA\Property(
+        title: "role_id",
+        type: "integer"
+    )]
+
+    public $role_id;
+
     public function authorize(): bool
     {
         // req gates for edit  Users
