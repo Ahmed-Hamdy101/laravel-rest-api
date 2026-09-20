e77ce949ee18015e26ddd614664c71abbf8d63a7
Ahmed-Hamdy101 <ahmedhamdy.mh95@gmail.com>
2026-09-20 14:19:15 +0300

    feat: enhance ProductCreateRequest with OpenAPI attributes for improved API documentation

--- app/Http/Requests/ProductCreateRequest.php
+++ app/Http/Requests/ProductCreateRequest.php
@@ -3,12 +3,56 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use OpenApi\Attributes as OA;
 
+
+#[OA\Schema(
+    title: 'Product',
+    description: 'Product model',
+ )]
+ 
 class ProductCreateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
+
+    #[OA\Property(
+            title: "title",
+            type: 'string'
+        )
+    ]
+
+    public string $title = '';
+
+
+    #[OA\Property(
+            title: "description",
+            type: 'string'
+        )
+    ]
+
+    public string $description;
+
+
+    #[OA\Property(
+            title: "image",
+            type: 'string'
+        )
+    ]
+
+    public string $image;
+
+    
+    #[OA\Property(
+        title: 'price',
+        type: 'number',
+        format: 'float',
+        minimum: 0
+    )]
+    public float $price;
+
+
     public function authorize(): bool
     {
         return true;
